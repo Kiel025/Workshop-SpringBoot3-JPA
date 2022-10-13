55b1101937ad606db594ea04ab28fac24c5103d8
kiel025 <kiel02505@gmail.com>
2022-10-13 11:46:41 -0300

    Service layer, component registration

--- src/main/java/me/udemy/course/repositories/UserRepository.java
+++ src/main/java/me/udemy/course/repositories/UserRepository.java
@@ -2,6 +2,8 @@ package me.udemy.course.repositories;
 
 import me.udemy.course.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 }
---
+++ src/main/java/me/udemy/course/services/UserService.java
@@ -0,0 +1,25 @@
+package me.udemy.course.services;
+
+import me.udemy.course.entities.User;
+import me.udemy.course.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public List<User> findALl() {
+        return userRepository.findAll();
+    }
+
+    public User findById(Long id) {
+        Optional<User> obj = userRepository.findById(id);
+        return obj.get();
+    }
+}
