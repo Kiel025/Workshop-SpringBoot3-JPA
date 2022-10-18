62fd64d30e011e139f68722d851a3615c362bbb7
kiel025 <kiel02505@gmail.com>
2022-10-17 23:36:26 -0300

    Exception handling - delete

--- src/main/java/me/udemy/course/services/UserService.java
+++ src/main/java/me/udemy/course/services/UserService.java
@@ -2,8 +2,11 @@ package me.udemy.course.services;
 
 import me.udemy.course.entities.User;
 import me.udemy.course.repositories.UserRepository;
+import me.udemy.course.services.exceptions.DatabaseException;
 import me.udemy.course.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -31,7 +34,13 @@ public class UserService {
     }
 
     public void delete(Long id) {
-        userRepository.deleteById(id);
+        try {
+            userRepository.deleteById(id);
+        } catch (EmptyResultDataAccessException e) {
+            throw new ResourceNotFoundException(id);
+        } catch (DataIntegrityViolationException e) {
+            throw new DatabaseException(e.getMessage());
+        }
     }
 
     public User update(Long id, User obj) {
---
+++ src/main/java/me/udemy/course/services/exceptions/DatabaseException.java
@@ -0,0 +1,13 @@
+package me.udemy.course.services.exceptions;
+
+import java.io.Serial;
+
+public class DatabaseException extends RuntimeException {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    public DatabaseException(String msg) {
+        super(msg);
+    }
+}
